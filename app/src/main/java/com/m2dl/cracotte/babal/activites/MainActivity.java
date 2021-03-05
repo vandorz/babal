16aa4499a4e3b8c17bced66d6475e7af3a98a55d
Heptaines-Romain <romain@goyheneix.fr>
2021-03-05 15:12:54 +0100

    RG : Ajout page menu basique

--- app/src/main/java/com/m2dl/cracotte/babal/activites/MainActivity.java
+++ app/src/main/java/com/m2dl/cracotte/babal/activites/MainActivity.java
@@ -7,20 +7,43 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.m2dl.cracotte.babal.R;
 
 import game.GameView;
 
 public class MainActivity extends Activity {
+    private Button playButton;
+    private Button leaveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        Intent gameIntent = new Intent().setClass(this, GameActivity.class);
-        startActivity(gameIntent);
+        setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents(){
+        initPlayButton();
+        initLeaveButton();
+    }
+
+    private void initPlayButton(){
+        playButton = findViewById(R.id.menu_button_lancerJeu);
+        System.out.println(playButton);
+        playButton.setOnClickListener(v -> {
+            Intent gameIntent = new Intent().setClass(v.getContext(), GameActivity.class);
+            startActivity(gameIntent);
+            finish();
+        });
+    }
+
+    private void initLeaveButton(){
+        leaveButton = findViewById(R.id.menu_button_quitterJeu);
+        leaveButton.setOnClickListener(v -> finish());
     }
 }
